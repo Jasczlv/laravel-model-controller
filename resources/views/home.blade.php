318b5eade32c5bbaa3d1a11974663f380afb7878
Jasczlv <ianis.cozlov@gmail.com>
2024-05-23 17:07:28 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('content')
+
+<div>
+    
+    @vite('resources/js/app.js')
+    <h1>
+        Home Page!
+    </h1>
+
+</div>
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ["resources/scss/app.scss", "resources/js/app.js"],
+            refresh: true,
+        }),
+    ],
+    //alias per i path
+    resolve: {
+        alias: {
+            "~resources": "/resources/",
+            "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+        },
+    },
+});
